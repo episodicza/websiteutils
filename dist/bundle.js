f86fef5d39321711b529e569c16d700ea9513b46
Will <will.vanderleij@episodic.co.za>
2023-09-11 14:50:56 +0200

    testing stuff

---
+++ dist/bundle.js
@@ -0,0 +1,12 @@
+(function () {
+  'use strict';
+
+  console.log("Looging in the root");
+
+  function main () {
+    console.log("Logging in the default");
+  }
+
+  return main;
+
+})();
---
+++ src/main.js
@@ -0,0 +1,5 @@
+console.log("Looging in the root");
+
+export default function () {
+  console.log("Logging in the default");
+}
