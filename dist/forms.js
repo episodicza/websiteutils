4098a90b1504e8aaf2d1939808f69ebf8fb842c5
Will <will.vanderleij@episodic.co.za>
2023-09-19 15:47:53 +0200

    erm

--- dist/forms.js
+++ dist/forms.js
@@ -2,7 +2,7 @@ var epsForms = (function (exports) {
   'use strict';
 
   function initFormValidation(formId, validationRules={}, fieldOptions={}, successHandler=null) {
-    let form = document.getElementById(formId);
+    let form = $(document.getElementById(formId));
 
     jQuery.validator.addMethod(
       "validPhoneNumber",
@@ -15,83 +15,80 @@ var epsForms = (function (exports) {
       }, "Invalid phone number"
     );
 
-    //Load ITI for the tel fields
-    const telInputs = form.querySelectorAll("input[type='tel']");
-    telInputs.forEach(function(telInput) {
-      let iti = window.intlTelInputGlobals.getInstance(telInput);
+    const telInputs = $("input[type='tel']", form);
+    telInputs.each(function() {
+      let iti = window.intlTelInputGlobals.getInstance(this);
       if (iti != null) { iti.destroy(); }
       const defaultConfig = {
         utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.1/build/js/utils.js",
         onlyCountries: ["za"],
         allowDropdown: true,
         autoPlaceholder: "aggressive",
-        hiddenInput: telInput.name + "_e164"
+        hiddenInput: this.name + "_e164"
       };
-      iti = window.intlTelInput(telInput, {...defaultConfig, ...fieldOptions});
+      iti = window.intlTelInput(this, {...defaultConfig, ...fieldOptions});
     });
 
     let baseOptions = {
       errorClass: "form-error",
       validClass: "form-valid",
       errorPlacement: function (error, element) {
-        if (element.parentElement.classList.contains("iti")) {
+        if (element.parent().hasClass("iti")) {
           error.insertAfter(element.parent());
         } else {
           error.insertAfter(element);
         }
       },
       highlight: function (element, errorClass, validClass) {
-        element.classList.add(errorClass).classList.remove(validClass);
+        $(element).addClass("field-error").removeClass("field-valid");
       },
       unhighlight: function (element, errorClass, validClass) {
-        element.classList.add(validClass).classList.remove(errorClass);
+        $(element).addClass("field-valid").removeClass("field-error");
       },
-      submitHandler: async function(form) {
-        const submitBtn = form.querySelector("input[type='submit']");
-        submitBtn.style.cursor = "progress";
-        submitBtn.disabled = true;
-        const doneBlock = form.parentElement.querySelector(".w-form-done");
-        const failBlock = form.parentElement.querySelector(".w-form-fail");
-        
-        let data = {
-          fields: Object.fromEntries(new FormData(formElem)),
+      submitHandler: function (formElement) {
+        const _form = $(formElement);
+        const submitBtn = _form.find("input[type='submit']");
+        submitBtn.prop("disabled", true).css("cursor", "progress");
+        const doneBlock = $(".w-form-done", _form.parent());
+        const failBlock = $(".w-form-fail", _form.parent());
+
+        const data = {
+          fields: Object.fromEntries(new FormData(formElement)),
           meta: { href: window.location.href },
         };
 
-        fetch(form.getAttribute("action"), {
-          method: form.getAttribute("method") || "POST", 
+        fetch(_form.attr("action"), {
+          method: _form.attr("method") || "POST", 
           headers: {
             "Content-Type": "application/json"
           },
           body: JSON.stringify(data)
         }).then(response => {
-          if (successCallback) {
-            successCallback(data);
+          if (successHandler) {
+            successHandler(data);
           }
-          const successRedirect = form.getAttribute("redirect") || form.dataset.redirect || null;
+          const successRedirect = _form.attr("redirect") || _form.data("redirect") || null;
           if (successRedirect) {
             window.location.href = successRedirect;
           } else {
-            form.style.display = 'none';
-            failBlock.style.display = 'none';
-            doneBlock.style.display = 'block';
+            _form.hide();
+            failBlock.hide();
+            doneBlock.show();
             $([document.documentElement, document.body]).animate({
               scrollTop: doneBlock.offset().top - 250,
             }, 300);
           }
-          submitBtn.disabled = false;
-          submitBtn.style.cursor = "default";
+          submitBtn.prop("disabled", false).css("cursor", "default");
         }).catch(error => {
           console.log(error);
-          form.style.display = 'block';
-          doneBlock.style.display = 'none';
-          failBlock.style.display = 'block';
-          submitBtn.disabled = false;
-          submitBtn.style.cursor = "default";
+          _form.show();
+          failBlock.show();
+          doneBlock.hide();
+          submitBtn.prop("disabled", false).css("cursor", "default");
         });
       }
     };
-    $(form).validate({ ...baseOptions, ...validationRules});
+    form.validate({ ...baseOptions, ...validationRules});
   }
 
   exports.initFormValidation = initFormValidation;
